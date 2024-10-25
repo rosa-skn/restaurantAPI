d1d534da667add7e1d52f7f8738b54ccc02126d2
rosa-skn <rosa.sokun@epitech.digital>
2024-10-25 11:40:26 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -21,62 +21,53 @@ dataBase.connect((err) =>{
     }
 });
 
-
 app.listen(expressport, () => {
-    console.log('mon serveur tourne sur le port:', expressport)
+    console.log('Server running on port:', expressport)
 });
-
-
 
 app.get('/items', (req, res)=>{
     const sql = 'SELECT * FROM items;';
     dataBase.query(sql,(err,results) =>{
      if(err){
-         return res.status(500).json({ error: 'ERREUR DU SERVEUR'});
+         return res.status(500).json({ error: 'SERVER ERROR'});
      } else{
-  return res.status(200).json(results);
+         return res.status(200).json(results);
      }
     });
- });
- 
- 
- app.post('/createitems', (req, res) => {
-     const { name, price, description } = req.body;
- 
-     const sql = 'INSERT INTO items (name, price, description) VALUES (?, ?, ?)';
-     
-     dataBase.query(sql, [name, price, description], (err, results) => {
-         if (err) {
-            return res.status(500).json({ error: 'ERREUR DU SERVEUR'});
-         } 
-         return res.status(200).json(results);
-     });
- });
- 
- 
- app.put('/updateitems', (req, res) => {
-     const { id, name, price, description } = req.body; 
- 
-     const sql = 'UPDATE items SET name = ?, price = ?, description = ? WHERE id = ?';
-     
-     dataBase.query(sql, [name, price, description, id], (err, results) => {
-         if (err) {
-             return res.status(500).json({ error: 'ERREUR DU SERVEUR'});
-         }
-         return res.status(200).json({ message: 'Item updated' });
-     });
- });
- 
- 
- 
- app.delete('/delete/:id', (req, res) => {
-     const id = req.params.id;
-     const sql = 'DELETE FROM items WHERE id = ?';
-     const value = [id];
-      dataBase.query(sql, value, (err, results) => {
-             if (err) {
-                 return res.status(500).json({ error: 'ERREUR DU SERVEUR' });
-             }
-             return res.status(200).json({ message: 'Item deleted' });
-         });
-     });
+});
+
+app.post('/createitems', (req, res) => {
+    const { name, price, description } = req.body;
+    const sql = 'INSERT INTO items (name, price, description) VALUES (?, ?, ?)';
+    
+    dataBase.query(sql, [name, price, description], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'SERVER ERROR' });
+        } 
+        return res.status(200).json(results);
+    });
+});
+
+app.put('/updateitems', (req, res) => {
+    const { id, name, price, description } = req.body;
+    const sql = 'UPDATE items SET name = ?, price = ?, description = ? WHERE id = ?';
+    
+    dataBase.query(sql, [name, price, description, id], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'SERVER ERROR'});
+        }
+        return res.status(200).json({ message: 'Item updated' });
+    });
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = 'DELETE FROM items WHERE id = ?';
+    
+    dataBase.query(sql, [id], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'SERVER ERROR' });
+        }
+        return res.status(200).json({ message: 'Item deleted' });
+    });
+});
